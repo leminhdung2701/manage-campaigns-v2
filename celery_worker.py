0abbc62bcc695c193f34396b7184eb2a686a2e9c
leminhdung2701 <64247437+leminhdung2701@users.noreply.github.com>
2022-07-15 13:54:12 +0700

    update time in sleep 2->20

--- celery_worker.py
+++ celery_worker.py
@@ -6,7 +6,7 @@ celery = Celery('tasks',broker='redis://redis:6379/0',backend='redis://redis:637
 @celery.task
 def calling(id_campaign,name_campaign,name_customer,phone):
     print("Calling "+ str(phone)+ " customer " + str(name_customer) + " in " + str(name_campaign)+" with id = "+str(id_campaign))
-    asyncio.sleep(2)
+    asyncio.sleep(20)
     return { 
             "name_customer":name_customer,
             "phone":phone,
